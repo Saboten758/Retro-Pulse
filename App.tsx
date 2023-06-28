6e56cb6e521a91d9ab42d19136df972acf67397c
Saboten758 <debjitdaw03@gmail.com>
2023-06-28 21:12:11 +0530

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React,{useState,useEffect} from 'react'
-import {Text,Button,View,StyleSheet,Alert, TouchableOpacity} from 'react-native'
+import {Text,Image,View,StyleSheet,Alert, TouchableOpacity} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator,NativeStackNavigationProp } from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/core'
 
 import { accelerometer,SensorTypes, setUpdateIntervalForType,magnetometer,gyroscope } from 'react-native-sensors';
 
@@ -8,7 +11,33 @@ setUpdateIntervalForType(SensorTypes.accelerometer, 400);
 setUpdateIntervalForType(SensorTypes.magnetometer, 400);
 setUpdateIntervalForType(SensorTypes.gyroscope, 400);
 
-const main=()=>{
+const Stack = createNativeStackNavigator();
+
+const app=()=>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={Home} name="Welcome"/>
+        <Stack.Screen component={Main} name="Work"/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+const Home=()=>{
+  const navigation=useNavigation();
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text3}>WELCOME</Text>
+      <Image source={require('./cassette.gif')} style={styles.giif}/>
+      <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('Work')}><Text style={styles.buttonText}>Sensors</Text></TouchableOpacity>
+    </View>
+
+  );
+}
+
+const Main=()=>{
   const [but,setbut]=useState(true);
   const [accel, setAccel] = useState({ x: 0, y: 0, z: 0, timestamp: 0 });
   const [magno,setmagno]=useState({x:0,y:0,z:0,timestamp:0})
@@ -59,23 +88,23 @@ const main=()=>{
       <View style={styles.sensor_data_cont}>
         <View style={styles.sensor_cont}>
           <Text style={styles.text2}>Accelerometer:</Text>
-          <Text>X: {!but? accel.x.toFixed(2):"Stopped"}</Text>
-          <Text>Y: {!but?accel.y.toFixed(2):"Stopped"}</Text>
-          <Text>Z: {!but?accel.z.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>X: {!but? accel.x.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>Y: {!but?accel.y.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>Z: {!but?accel.z.toFixed(2):"Stopped"}</Text>
         </View>
 
         <View style={styles.sensor_cont}>
           <Text style={styles.text2}>Magnetometer:</Text>
-          <Text>X: {!but? magno.x.toFixed(2):"Stopped"}</Text>
-          <Text>Y: {!but?magno.y.toFixed(2):"Stopped"}</Text>
-          <Text>Z: {!but?magno.z.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>X: {!but? magno.x.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>Y: {!but?magno.y.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>Z: {!but?magno.z.toFixed(2):"Stopped"}</Text>
         </View>
 
         <View style={styles.sensor_cont}>
           <Text style={styles.text2}>Gyroscope:</Text>
-          <Text>X: {!but? gyro.x.toFixed(2):"Stopped"}</Text>
-          <Text>Y: {!but?gyro.y.toFixed(2):"Stopped"}</Text>
-          <Text>Z: {!but?gyro.z.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>X: {!but? gyro.x.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>Y: {!but?gyro.y.toFixed(2):"Stopped"}</Text>
+          <Text style={styles.text3}>Z: {!but?gyro.z.toFixed(2):"Stopped"}</Text>
         </View>
          
       </View>
@@ -84,11 +113,14 @@ const main=()=>{
   );
 };
 
-export default main;
+
+
+
+export default app;
 
 const styles=StyleSheet.create({
   container:{
-    padding:10,alignItems:'center',justifyContent:'center',flex:1,color:'red'
+    padding:10,alignItems:'center',justifyContent:'center',flex:1,backgroundColor:"#6B7A8F"
   },
   text:{
     padding:10,fontSize:43,color:'red',
@@ -97,7 +129,7 @@ const styles=StyleSheet.create({
   text2:{
     paddingTop:20,
     fontWeight:'bold',
-    color:'#6EC7B9'
+    color:'#BBA9C3'
   },
   sensor_data_cont:{
     flexDirection:'row',
@@ -109,7 +141,7 @@ const styles=StyleSheet.create({
     padding:20
   },
   button: {
-    backgroundColor: '#6EC7B9',
+    backgroundColor: '#BBA9C3',
     borderRadius: 10,
     paddingVertical: 12,
     paddingHorizontal: 24,
@@ -121,4 +153,15 @@ const styles=StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+  text3:{
+    color:'black'
+  },
+  giif:{
+    padding:10,
+    height:450,
+    width:450,
+    marginBottom:10,
+    marginTop:10,
+    borderRadius:3,
+  }
 });
