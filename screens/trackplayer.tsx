7d952ce49220940c222b3e18b9039874ee192e3b
Saboten758 <debjitdaw03@gmail.com>
2023-07-01 17:43:20 +0530

    added Nightwave Plaza Api for music

--- screens/trackplayer.tsx
+++ screens/trackplayer.tsx
@@ -4,6 +4,31 @@ import TrackPlayer, {
   RepeatMode,
   Event
 } from 'react-native-track-player';
+import axios from 'axios';
+import { ToastAndroid } from 'react-native';
+async function fetchCurrentSongId() {
+  try {
+    const response = await axios.get('https://api.plaza.one/status');
+    const currentSongId = response.data.song.id;
+    return currentSongId;
+  } catch (error) {
+    console.error('Error fetching current song ID:', error);
+    return null;
+  }
+}
+const showTost = (title) => {
+  ToastAndroid.show(title+' was added to playlist!!', ToastAndroid.SHORT);
+};
+async function fetchSongInfo(songId) {
+  try {
+    const response = await axios.get(`https://api.plaza.one/songs/${songId}`);
+    const song = response.data;
+    return song;
+  } catch (error) {
+    console.error('Error fetching song information:', error);
+    return null;
+  }
+}
 
 export async function setupPlayer() {
   let isSetup = false;
@@ -39,34 +64,59 @@ export async function setupPlayer() {
     return isSetup;
   }
 }
-
+const addedSongIds = [];
 export async function addTracks() {
   await TrackPlayer.add([
-    {
-      id: '1',
-      url: require('./assets/fluidity-100-ig-edit-4558.mp3'),
-      title: 'Fluidity',
-      artist: 'tobylane',
-      duration: 60,
-    },
-    {
-      id: '2',
-      url: require('./assets/penguinmusic-modern-chillout-future-calm-12641.mp3'),
-      title: 'Modern Chillout',
-      artist: 'penguinmusic',
-      duration: 66,
-    },
-    {
-      id: '3',
-      url: require('./assets/powerful-beat-121791.mp3'),
-      title: 'Powerful Beat',
-      artist: 'penguinmusic',
-      duration: 73,
-    }
-  ]);
-  await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+      {
+        id: '1',
+        url: require('./assets/fluidity-100-ig-edit-4558.mp3'),
+        title: 'Fluidity',
+        artist: 'tobylane',
+        duration: 60,
+      },
+      {
+        id: '2',
+        url: require('./assets/penguinmusic-modern-chillout-future-calm-12641.mp3'),
+        title: 'Modern Chillout',
+        artist: 'penguinmusic',
+        duration: 66,
+      },
+      {
+        id: '3',
+        url: require('./assets/powerful-beat-121791.mp3'),
+        title: 'Powerful Beat',
+        artist: 'penguinmusic',
+        duration: 73,
+      }
+    ]);
+    await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+  
 }
+async function Nightwave(){
+  const currentSongId = await fetchCurrentSongId();
+  if (currentSongId) {
+    const currentSongInfo = await fetchSongInfo(currentSongId);
+    if (addedSongIds.includes(currentSongInfo.id)) {
+      return;
+    }
+    addedSongIds.push(currentSongInfo.id)
+    if (currentSongInfo) {
+      const track = {
+        id: currentSongInfo.id,
+        url: currentSongInfo.preview_src,
+        title: currentSongInfo.title,
+        artist: currentSongInfo.artist,
+        duration: currentSongInfo.length,
+        artwork: currentSongInfo.artwork_src,
+      };
+      console.log(track)
 
+      await TrackPlayer.add(track);
+      showTost(currentSongInfo.title);
+      await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+    }
+  }
+}
 export async function playbackService() {
   TrackPlayer.addEventListener(Event.RemotePause, () => {
     console.log('Event.RemotePause');
@@ -88,3 +138,6 @@ export async function playbackService() {
     TrackPlayer.skipToPrevious();
   });
 }
+setInterval(async () => {
+  await Nightwave();
+}, 30000);
